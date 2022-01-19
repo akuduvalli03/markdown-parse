738da901efdb58e8298f76b6b34bc6289b353e53
Akshara Kuduvalli <akshara.kuduvalli@gmail.com>
2022-01-19 14:44:38 -0800

    testing currentIndex increment

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,12 +17,15 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
+            
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
+        System.out.println(contents);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
