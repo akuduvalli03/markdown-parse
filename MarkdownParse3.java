15f8286e73cca3aafb72e17efb1bd7ea1764b1c1
Akshara Kuduvalli <akshara.kuduvalli@gmail.com>
2022-01-26 18:10:30 -0800

    update

--- MarkdownParse3.java
+++ MarkdownParse3.java
@@ -3,43 +3,54 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MarkdownParse3 {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
+        ArrayList<Integer> periodList = new ArrayList<Integer>();
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            if(nextOpenBracket == -1) {
+            int nextPeriodIndex = markdown.indexOf(".", currentIndex + 1);
+            if(nextPeriodIndex != -1) {
+                periodList.add(nextPeriodIndex);
+                currentIndex = nextPeriodIndex;
+            } else {
                 break;
             }
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            if(openParen > markdown.indexOf("\n", nextCloseBracket) && markdown.indexOf("\n", nextCloseBracket) != -1) {
-                openParen = -1;
+        }
+        for(int periodIndex: periodList) {
+            int startIndex = periodIndex;
+            int endIndex = periodIndex;
+            ArrayList<Character> stopCharacters = new ArrayList<Character>();
+            stopCharacters.add('(');
+            stopCharacters.add(')');
+            stopCharacters.add('[');
+            stopCharacters.add(']');
+            stopCharacters.add('\n');
+            while(startIndex >= 0) {
+                if(stopCharacters.contains(markdown.charAt(startIndex))) {
+                    startIndex++;
+                    break;
+                }
+                startIndex--;
             }
-            if(openParen == -1) {
-                openParen = nextCloseBracket;
+            while(endIndex < markdown.length()) {
+                if(stopCharacters.contains(markdown.charAt(endIndex))) {
+                    endIndex--;
+                    break;
+                }
+                endIndex++;
             }
-
-            int closeParen = markdown.indexOf(")", openParen);
-            if(closeParen > markdown.indexOf("\n", openParen) && markdown.indexOf("\n", openParen) != -1) {
-                closeParen = -1;
-            }
-            if(closeParen == -1) {
-                closeParen = markdown.indexOf("\n", openParen);
-            }
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            // System.out.println("NOB: " + nextOpenBracket + " NCB: " + nextCloseBracket + " OP: " + openParen + " CP: " + closeParen);
-            currentIndex = closeParen + 1;
+            toReturn.add(markdown.substring(startIndex, endIndex + 1));
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         // System.out.println(contents);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
