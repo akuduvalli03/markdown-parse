1c149fdde5c9bffd161190bba3ad979e868f7e39
Akshara Kuduvalli <akshara.kuduvalli@gmail.com>
2022-02-07 18:58:30 -0800

    reflect till error1

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,9 +10,7 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        
-        markdown = markdown.replace("\n(","\n[");
-        markdown = markdown.replace(")(","]("); /*
+        /*
         //for when there aren't parentheses around the link
         if(markdown.indexOf("]",currentIndex)!= markdown.indexOf("](",currentIndex)) {
             //when there isn't parentheses starting the link
@@ -29,6 +27,11 @@ public class MarkdownParse {
         }
 
         System.out.println(markdown);*/
+
+
+        markdown = markdown.replace("\n(","\n[");
+        markdown = markdown.replace(")(","](");  
+        
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
